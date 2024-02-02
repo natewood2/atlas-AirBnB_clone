e8f539a02bf96c251f657712794e2f5c8761681a
natewood2 <nathanwood.sfg@gmail.com>
2024-02-01 19:38:18 -0600

    added the initial console.py file

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" THE CONSOLE. """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ cmd module. """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+            return True
+
+    def do_EOF(self, arg):
+        return True
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
