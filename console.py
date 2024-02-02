6b448d5af5cf0d7ff28fee36cf0bd2ae6386614b
natewood2 <nathanwood.sfg@gmail.com>
2024-02-01 20:16:37 -0600

    adding my progress before I wrap up for the night

--- console.py
+++ console.py
@@ -7,6 +7,23 @@ class HBNBCommand(cmd.Cmd):
     """ cmd module. """
     prompt = '(hbnb) '
 
+    def create(self, args):
+        """ Creates a new instance of BaseModel, saves it 
+        (to the JSON file) and prints the id. 
+        """
+        arguments = args.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        try:
+            instance = eval(arguments[0])
+            instance.save()
+            print(instance.id)
+        except:
+            print("** class doesn't exist **")
+
     def do_quit(self, arg):
             return True
 
