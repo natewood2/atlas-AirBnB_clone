96190e34ffa45b16be1efa92750218de16ed3670
natewood2 <nathanwood.sfg@gmail.com>
2024-02-02 22:07:35 -0600

    the new instance of save is missing so I am adding it back.

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+        models.storage.new(self)
 
     def __str__(self):
         """ Returns a string representation of the instance. """
