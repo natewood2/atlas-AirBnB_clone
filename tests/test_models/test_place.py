b029b3abb6e0dcdffcccffead556aef64766595d
natewood2 <143881431+natewood2@users.noreply.github.com>
2024-02-05 19:51:07 -0600

    Update test_place.py
    getting rid of my hard work

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -19,24 +19,13 @@ class TestPlace(unittest.TestCase):
     def test_variable_attributes(self):
         """ Testing values and equals. """
         place = Place()
-        place.city_id = "Tulsa"
-        place.user_id = "atlasStudent"
-        place.name = "Nathan"
-        place.description = "Awesome City"
-        place.number_rooms = "3"
-        place.number_bathrooms = "45"
-        place.max_guest = "3"
-        place.price_by_night = "$235"
-        place.latitude = "37.7749"
-        place.longitude = "-122.4194"
-
-        self.assertEqual(place.city_id, "Tulsa")
-        self.assertEqual(place.user_id, "atlasStudent")
-        self.assertEqual(place.name, "Nathan")
-        self.assertEqual(place.description, "Awesome City")
-        self.assertEqual(place.number_rooms, "3")
-        self.assertEqual(place.number_bathrooms, "45")
-        self.assertEqual(place.max_guest, "3")
-        self.assertEqual(place.price_by_night, "$235")
-        self.assertEqual(place.latitude, "37.7749")
-        self.assertEqual(place.longitude, "-122.4194")
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
