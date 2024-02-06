e47d5c7e8a45e639bede376fcfd36a17b9a9180b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-05 22:33:54 -0600

    Filestorage save test working locally

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,34 +8,30 @@ from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
     """ Testing File storage. """
-    def setUp(self):
-        """ Setting up. """
-        self.test_instance = BaseModel()
-        self.test_instance.save()
-        self.saving_test = FileStorage()
-        storage.reload()
-
-    def tearDown(self):
-        if os.path.exists("file.json"):
-            os.remove("file.json")
 
     def test_save(self):
         """ Testing save. """
-        all_objects = storage.all()
-        object_key = f"{self.test_instance.__class__.__name__}.{self.test_instance.id}"
-        self.assertIn(object_key, all_objects)
-        self.assertTrue(hasattr(self.test_instance, 'save'))
-        tmp = self.test_instance.updated_at.microsecond
-        self.test_instance.save()
-        self.assertNotEqual(tmp,
-                            self.test_instance.updated_at.microsecond)
+        instance = FileStorage()
+        obj = BaseModel()
+        path = "file.json"
+        with open(path, 'r') as f:
+            before_save_content = json.load(f)
+
+        instance.all()["New"] = obj
+        instance.save()
+
+        with open(path, 'r') as f:
+            after_save_content = json.load(f)
+
+        self.assertNotEqual(before_save_content, after_save_content)
 
 
     def test_new_and_all(self):
         """ Test that new objects are correctly added to storage. """
         new_object = BaseModel()
-        self.saving_test.new(new_object)
-        all_objects = self.saving_test.all()
+        tmp = FileStorage()
+        tmp.new(new_object)
+        all_objects = storage.all()
         object_key = f"{new_object.__class__.__name__}.{new_object.id}"
         self.assertIn(object_key, all_objects)
         self.assertEqual(all_objects[object_key], new_object)
